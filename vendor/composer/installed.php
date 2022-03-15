67d9a3a1a2a468eaf1c6e7d27b2e4641c45a28de
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-03-15 12:02:15 +1100

    Add S3 Uploads plugin files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '504d607ddcff4989bc77662a82cd6b636966b368',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '504d607ddcff4989bc77662a82cd6b636966b368',
+            'dev_requirement' => false,
+        ),
+    ),
+);
