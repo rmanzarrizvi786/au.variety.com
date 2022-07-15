884e52b6a9959b065faa2be6c65906782e55d8ac
Sachin Patel <71243148+sachtbm@users.noreply.github.com>
2022-07-15 10:13:28 +1000

    add data attribute

--- themes/tbm-var/pmc/template-parts/patterns/components/c-timestamp.php
+++ themes/tbm-var/pmc/template-parts/patterns/components/c-timestamp.php
@@ -1,12 +1,7 @@
 <?php
 // This is a generated file. Refer to the relevant Twig file for adjusting this markup.
 ?>
-<time class="c-timestamp <?php echo esc_attr( $modifier_class ?? '' ); ?> <?php echo esc_attr( $c_timestamp_classes ?? '' ); ?>"
-	datetime="<?php echo esc_attr( $c_timestamp_datetime_attr ?? '' ); ?>"
-	<?php if ( ! empty( $c_timestamp_aria_label_attr ) ) { ?>
-		aria-label="<?php echo esc_attr( $c_timestamp_aria_label_attr ?? '' ); ?>"
-	<?php } ?>
-	>
-	<?php echo esc_html( $c_timestamp_text ?? '' ); ?>
+<time class="c-timestamp <?php echo esc_attr($modifier_class ?? ''); ?> <?php echo esc_attr($c_timestamp_classes ?? ''); ?>" datetime="<?php echo esc_attr($c_timestamp_datetime_attr ?? ''); ?>" <?php if (!empty($c_timestamp_aria_label_attr)) { ?> aria-label="<?php echo esc_attr($c_timestamp_aria_label_attr ?? ''); ?>" <?php } ?> data-pubdate="<?php echo get_the_time('M d, Y'); ?>">
+	<?php echo esc_html($c_timestamp_text ?? ''); ?>
 
 </time>
