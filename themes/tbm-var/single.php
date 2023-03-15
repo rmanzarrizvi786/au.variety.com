ee6b882ce089ec967b256cda8cbd33891dcfc8ce
Toby Smith <tobypsmith@gmail.com>
2023-03-16 10:21:38 +1100

    feat: better password protect

--- themes/tbm-var/single.php
+++ themes/tbm-var/single.php
@@ -6,6 +6,18 @@
  * @package pmc-variety
  */
 
+ if (post_password_required($post)) {
+	if (is_user_logged_in()) {
+		do_action( 'wp_footer','wp_admin_bar_render' );
+	}
+
+	get_template_part('template-parts/protected/header');
+		echo get_the_password_form();
+	get_template_part('template-parts/protected/footer');
+
+	return '';
+}
+
 get_header();
 
 $single_template = \Variety\Inc\Featured_Article::get_instance()->is_featured_article() ? 'single-featured' : 'single';
