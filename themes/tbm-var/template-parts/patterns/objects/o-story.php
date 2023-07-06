2163506fc41ce3576ea3b2a40a46361050bb8857
Toby Smith <tobypsmith@gmail.com>
2023-07-06 18:13:33 +1000

    testing image swap

--- themes/tbm-var/template-parts/patterns/objects/o-story.php
+++ themes/tbm-var/template-parts/patterns/objects/o-story.php
@@ -36,9 +36,30 @@
 	<?php if (!empty($c_lazy_image)) { ?>	
 		<div class="o-story__secondary lrv-a-glue-parent // <?php echo esc_attr($o_story_secondary_classes ?? ''); ?>">
 			<?php if((int) $c_lazy_image['post_id'] == (int) 9536) { ?>
-				<!-- "https://images.thebrag.com/var/uploads/2023/07/gary-vee.jpg" -->
-				<?php $c_lazy_image_src_url = "https://images.thebrag.com/var/uploads/2023/07/gary-vee.jpg"; ?>
-			<?php } ?>
+				<div class="c-lazy-image <?php echo esc_attr($modifier_class ?? ''); ?> <?php echo esc_attr($c_lazy_image_classes ?? ''); ?>">
+				<?php if (!empty($c_lazy_image_link_url)) { ?>
+				<a href="<?php echo esc_url($c_lazy_image_link_url ?? ''); ?>" class="c-lazy-image__link <?php echo esc_attr($c_lazy_image_link_classes ?? ''); ?>">
+				<?php } ?>
+
+				<?php if (!empty($c_lazy_image_crop_class)) { ?>
+				<div class="<?php echo esc_attr($c_lazy_image_crop_class ?? ''); ?>" style="<?php echo esc_attr($c_lazy_image_crop_style_attr ?? ''); ?>">
+				<?php } ?>
+
+				<?php if (!empty($c_lazy_image_markup)) { ?>
+				<?php echo wp_kses_post($c_lazy_image_markup ?? ''); ?>
+				<?php } else { ?>
+				<img class="c-lazy-image__img <?php echo esc_attr($c_lazy_image_img_classes ?? ''); ?>" src="https://images.thebrag.com/var/uploads/2023/07/gary-vee.jpg" alt="<?php echo esc_attr($c_lazy_image_alt_attr ?? ''); ?>" data-lazy-srcset="<?php echo esc_attr($c_lazy_image_srcset_attr ?? ''); ?>" data-lazy-sizes="<?php echo esc_attr($c_lazy_image_sizes_attr ?? ''); ?>" height="<?php echo esc_attr($c_lazy_image_height_attr ?? ''); ?>" width="<?php echo esc_attr($c_lazy_image_width_attr ?? ''); ?>">
+				<?php } ?>
+
+				<?php if (!empty($c_lazy_image_crop_class)) { ?>
+				</div>
+				<?php } ?>
+
+				<?php if (!empty($c_lazy_image_link_url)) { ?>
+				</a>
+				<?php } ?>
+				</div>
+			<?php } else { ?>
 
 			<?php if (!empty($c_lazy_image)) { ?>
 				<?php \PMC::render_template(PMC_CORE_PATH . '/template-parts/patterns/components/c-lazy-image.php', $c_lazy_image, true); ?>
@@ -49,7 +70,7 @@
 					</a>
 				<?php } ?>
 			<?php } ?>
-
+			<?php } ?>
 		</div>
 	<?php } ?>
 
