a4f1e203301c43afd7815d958fa002389dcd4618
Toby Smith <tobypsmith@gmail.com>
2023-07-06 17:44:07 +1000

    testing image swap

--- themes/tbm-var/pmc/template-parts/patterns/components/c-lazy-image.php
+++ themes/tbm-var/pmc/template-parts/patterns/components/c-lazy-image.php
@@ -1,6 +1,10 @@
 <?php
 // This is a generated file. Refer to the relevant Twig file for adjusting this markup.
 ?>
+<?php if($c_lazy_image['post_id'] == 9536) {
+	$c_lazy_image_src_url = "https://images.thebrag.com/var/uploads/2023/07/gary-vee.jpg";
+	echo "Gary Vee 123";
+} ?>
 <div class="c-lazy-image <?php echo esc_attr($modifier_class ?? ''); ?> <?php echo esc_attr($c_lazy_image_classes ?? ''); ?>">
 	<?php if (!empty($c_lazy_image_link_url)) { ?>
 		<a href="<?php echo esc_url($c_lazy_image_link_url ?? ''); ?>" class="c-lazy-image__link <?php echo esc_attr($c_lazy_image_link_classes ?? ''); ?>">
@@ -13,9 +17,6 @@
 			<?php if (!empty($c_lazy_image_markup)) { ?>
 				<?php echo wp_kses_post($c_lazy_image_markup ?? ''); ?>
 			<?php } else { ?>
-				<?php if($c_lazy_image['post_id'] == 9536) { ?>
-					<?php $c_lazy_image_src_url = "https://images.thebrag.com/var/uploads/2023/07/gary-vee.jpg"; ?>
-				<?php } ?>
 				<img class="c-lazy-image__img <?php echo esc_attr($c_lazy_image_img_classes ?? ''); ?>" src="<?php echo esc_url($c_lazy_image_src_url ?? ''); ?>" alt="<?php echo esc_attr($c_lazy_image_alt_attr ?? ''); ?>" data-lazy-srcset="<?php echo esc_attr($c_lazy_image_srcset_attr ?? ''); ?>" data-lazy-sizes="<?php echo esc_attr($c_lazy_image_sizes_attr ?? ''); ?>" height="<?php echo esc_attr($c_lazy_image_height_attr ?? ''); ?>" width="<?php echo esc_attr($c_lazy_image_width_attr ?? ''); ?>">
 			<?php } ?>
 
