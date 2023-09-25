3f57a547f3ab8e5c966493d72f0da71da611855a
Toby Smith <tobypsmith@gmail.com>
2023-09-25 17:44:33 +1000

    updating excerpt trim

--- themes/tbm-var/functions.php
+++ themes/tbm-var/functions.php
@@ -788,15 +788,14 @@ add_action( 'pre_get_posts', 'add_custom_post_type_to_query' );
 function tbm_the_excerpt( $excerpt ) {
     $excerpt = str_replace( ' [&hellip;]', '', $excerpt );
     $excerpt = str_replace( 'St. ', 'St*& ', $excerpt );
-
     $excerpt = str_replace( 'aka. ', 'aka*& ', $excerpt );
     $excerpt = str_replace( 'a.k.a. ', 'a*&a*&a ', $excerpt );
     $excerpt = str_replace( 'a.k.a ', 'a*&a*&a ', $excerpt );
-
     $excerpt = str_replace( 'M.I.A. ', 'm*&i*&a ', $excerpt );
     $excerpt = str_replace( 'M.I.A ', 'm*&i*&a ', $excerpt );
-
     $excerpt = str_replace( 'Dr. ', 'Dr*& ', $excerpt );
+    $excerpt = str_replace( 'L.A.B ', 'L*&A*&B ', $excerpt ); 
+    $excerpt = str_replace( 'L.A.B. ', 'L*&A*&B*& ', $excerpt ); 
 
     $excerpt = explode('// ', $excerpt);
     $excerpt = count( $excerpt ) > 1 ? $excerpt[1] : $excerpt[0];
@@ -810,6 +809,8 @@ function tbm_the_excerpt( $excerpt ) {
     $excerpt = str_replace( 'a*&a*&a ', 'a.k.a. ', $excerpt );
     $excerpt = str_replace( 'm*&i*&a ', 'M.I.A. ', $excerpt );
     $excerpt = str_replace( 'Dr*& ', 'Dr. ', $excerpt );
+    $excerpt = str_replace( 'L*&A*&B ', 'L.A.B ', $excerpt );
+    $excerpt = str_replace( 'L*&A*&B*& ', 'L.A.B. ', $excerpt );
 
     return $excerpt . '.';
 }
